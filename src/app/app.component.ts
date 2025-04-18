27d3d6ffb5a4006f839bca6af833bb12ff7eb98a
Adrian Krynicki <adrian.krynicki88@gmail.com>
2025-04-19 00:50:03 +0200

    feat: add portfolio component with responsive design and project cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from './hero/hero.component';  
+import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './aboutMe/aboutMe.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, AboutMeComponent, SkillsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
